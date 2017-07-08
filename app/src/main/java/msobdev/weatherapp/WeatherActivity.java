ae0fdd7fc5a7b3c731fa3f2ab79f90d57206e5b3
msobdev <msobdev@gmail.com>
2017-07-08 02:20:58 +0200

    LocationListener early implementation and layout activity

--- app/src/main/java/msobdev/weatherapp/WeatherActivity.java
+++ app/src/main/java/msobdev/weatherapp/WeatherActivity.java
@@ -1,13 +1,65 @@
 package msobdev.weatherapp;
 
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class WeatherActivity extends AppCompatActivity {
+import msobdev.weatherapp.Model.OpenWeatherMap;
+
+public class WeatherActivity extends AppCompatActivity implements LocationListener{
+
+    TextView txtCity, txtLastUpdate, txtDescription, txtHumidity, txtTime, txtCelcius;
+    ImageView imageView;
+
+    LocationManager locationManager;
+    String provider;
+    static double lat, lng;
+    OpenWeatherMap openWeatherMap = new OpenWeatherMap();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
     }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
